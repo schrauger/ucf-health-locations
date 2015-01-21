dd7e2072502f3f89b47cd849e54350153ef046b1
Stephen Schrauger <github@schrauger.com>
2015-01-21 12:08:54 -0500

    Require external project

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ Plugin Name: UCF Health locations taxonomy
+Plugin URI: https://github.com/schrauger/ucf-health-locations
+Description: A custom taxonomy with custom fields for latitude, longitude, phone, etc.
+Version: 0.1
+Author: Stephen Schrauger
+Author URI: https://www.schrauger.com/
+License: GPLv2 or later
+*/
+
+/**
+ * Settings|config page for plugin
+ */
+
+require_once(get_plugin_directory() . "/Tax-meta-class/Tax-meta-class/Tax-meta-class.php"); // lets the 'location' taxonomy have custom fields for lat/long etc
+
+?>
