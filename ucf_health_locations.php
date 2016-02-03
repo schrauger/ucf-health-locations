9b89cd476201cdc65f70fa24364f8a5b2c3fa9de
Stephen Schrauger <stephen@schrauger.com>
2016-02-03 10:21:19 -0500

    fixes telephone links

--- ucf_health_locations.php
+++ ucf_health_locations.php
@@ -387,7 +387,7 @@ class ucf_health_locations {
 		$return .= "	<ul class=''>";
 		$return .= "		<div class='third'>";
 		$return .= "			<h2>" . nl2br( $location->name ) . "</h2>";
-		$return .= "			<strong>Address:</strong><br />";
+		$return .= "			<strong>Address:</srong><br />";
 		$return .= "			<p>" . nl2br( $location->address ) . "</p>";
 		$return .= " 			<p><strong>Directions:</strong></p>";
 		$return .= "			<a href='" . $this->get_directions( $location ) . "' class='green map location' target='_blank'>Google Maps</a>";
@@ -398,7 +398,7 @@ class ucf_health_locations {
 		$return .= "		</div>";
 		$return .= "		<div class='third'>";
 		$return .= "			<strong>Phone:</strong><br />";
-		$return .= "			<p>" . nl2br( $location->phone_number ) . "</p>";
+		$return .= "			<p>" . nl2br( stripslashes($location->phone_number )) . "</p>";
 		$return .= "			<strong>Fax:</strong><br />";
 		$return .= "			<p>" . nl2br( $location->fax_number ) . "</p>";
 		$return .= "		</div>";
